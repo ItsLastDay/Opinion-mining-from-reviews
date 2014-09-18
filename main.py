7bae7a32e6ed34a9b1494923b9cbec5f4890d0e5
Mikhail Koltsov <misha@koltsov.su>
2014-09-18 11:53:32 +0400

    Written Solution.train

--- main.py
+++ main.py
@@ -1,11 +1,10 @@
 from data_extractor import get_nice_data
-#from solution import Solution
+from solution import Solution
 from sklearn import metrics, cross_validation
 
 
 train_data = get_nice_data('reviews.json')
-print train_data[1]
-#sol = Solution()
+sol = Solution()
 
 #scores = cross_validation.cross_val_score(sol, train_data[0], train_data[1], \
 #        cv=3, scoring='f1')
--- solution.py
+++ solution.py
@@ -2,14 +2,28 @@ from sklearn.multiclass import OneVsRestClassifier
 import numpy as np
 from sklearn.naive_bayes import MultinomialNB
 from sklearn.preprocessing import label_binarize
+from nltk import word_tokenize
 
 class Solution:
+    _ngram = 3
+
     def __init__(self):
         self._opinion_to_number = dict()
         self._ngram_to_number = dict()
         self._n_fearures = 0
         self._clf = OneVsRestClassifier(MultinomialNB(), n_jobs=-1) 
     
+    def get_params(self):
+        return self._clf.get_params(deep=True)
+
+    @staticmethod
+    def _normalize_text(text):
+        return text.lower()
+
+    def _ngr_add(self, ngram):
+        if ngram not in self._ngram_to_number:
+            self._ngram_to_number[ngram] = len(self._ngram_to_number)
+
     def _encode_opinions(self, op_list):
         ''' 
             Transforms opinion list like [[('ggg', positive), 
@@ -29,18 +43,33 @@ class Solution:
 
         return label_binarize(converted_op_list, classes=range(n_ops))
         
-
     def train(self, train_corp):
         texts = train_corp[0]
-        target = self._encode_opinions(train_corp[1])
 
+        target = self._encode_opinions(train_corp[1])
         features_list = []
 
-
+        token_list = []
         for text in texts:
-            pass
+            ntext = Solution._normalize_text(text)
+            
+            tokens = word_tokenize(ntext)
+            token_list.append([])
+            for token in tokens:
+                tk = '^' + token + '$'
+                token_list[-1].append(tk)
+                for i in xrange(len(tk) - Solution._ngram + 1):
+                    self._ngr_add(tk[i:i + Solution._ngram])
 
-        return self
+        n_features = len(self._ngram_to_number)
+        for tokens in token_list:
+            features_list.append([0 for i in xrange(n_features)])
+
+            for tk in tokens:
+                features_list[-1][self._ngram_to_number[tk]] += 1
+
+        self._clf.fit(features_list, target)
+
 
     def fit(self, x, y):
         self.train((x, y))

182bb928a42f2d65a5784508e3f98ed162a6ade8
Mikhail Koltsov <misha@koltsov.su>
2014-09-18 11:39:59 +0400

    Wrote target label transformation to binary matrix.

--- main.py
+++ main.py
@@ -1,7 +1,11 @@
 from data_extractor import get_nice_data
-from solution import Solution
+#from solution import Solution
+from sklearn import metrics, cross_validation
 
 
 train_data = get_nice_data('reviews.json')
-sol = Solution()
-sol.train(train_data)
+print train_data[1]
+#sol = Solution()
+
+#scores = cross_validation.cross_val_score(sol, train_data[0], train_data[1], \
+#        cv=3, scoring='f1')
--- solution.py
+++ solution.py
@@ -1,15 +1,52 @@
-import sklearn
+from sklearn.multiclass import OneVsRestClassifier
+import numpy as np
+from sklearn.naive_bayes import MultinomialNB
+from sklearn.preprocessing import label_binarize
 
 class Solution:
     def __init__(self):
-        pass
+        self._opinion_to_number = dict()
+        self._ngram_to_number = dict()
+        self._n_fearures = 0
+        self._clf = OneVsRestClassifier(MultinomialNB(), n_jobs=-1) 
+    
+    def _encode_opinions(self, op_list):
+        ''' 
+            Transforms opinion list like [[('ggg', positive), 
+            ('fff', negative)], [('qqq', eutral)]] into
+            [[1, 1, 0], [0, 0, 1]]
+        '''
+        n_ops = 0
+        converted_op_list = []
+        for ops in op_list:
+            for op in ops:
+                if op not in self._opinion_to_number:
+                    self._opinion_to_number[op] = n_ops
+                    n_ops += 1
+
+            converted_op_list.append(tuple(map(lambda x: \
+                    self._opinion_to_number[x], ops)))
+
+        return label_binarize(converted_op_list, classes=range(n_ops))
+        
 
     def train(self, train_corp):
         texts = train_corp[0]
-        target = train_corp[1]
+        target = self._encode_opinions(train_corp[1])
+
+        features_list = []
+
 
         for text in texts:
+            pass
 
+        return self
+
+    def fit(self, x, y):
+        self.train((x, y))
+        return self
+
+    def predict(self, text):
         pass
 
     def getClasses(self, texts):
